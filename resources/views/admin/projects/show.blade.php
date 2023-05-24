71592f3b4a68a64e0b3ef5d20d3f17cb8a22c514
Andrea Fermo <andreafermo.sd@gmail.com>
2023-05-24 15:59:16 +0200

    Aggiunto visualizzazione tipologia in project/show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -2,6 +2,7 @@
 @section('page-title', $project->title)
 @section('content')
     <h1 class="text-center mt-3 mb-5">Titolo: {{$project->title}}</h1>
+    <h3 class="text-center mt-3 mb-5">Tipologia: {{$project->type?$project->category->name:'Nessuna tipologia assegnata'}}</h3>
     <p class="ms-3 me-3"><span class="fw-bold">Descrizione:</span> {{$project->description}}</p>
     <a href="{{route('admin.projects.index')}}" class="btn btn-primary">Torna alla lista progetti</a>
 @endsection
